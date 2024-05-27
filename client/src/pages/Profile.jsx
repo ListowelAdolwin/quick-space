a8842d675d1f80d94200894171971391d737ffc5
ListowelAdolwin <listoweladolwin>
2024-05-27 13:15:49 +0000

    Designed the profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { logoutUser } from "../redux/features/user/userSlice";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { CiViewList } from "react-icons/ci";
 
 const UserProfile = () => {
 	const [userData, setUserData] = useState(null);
@@ -73,20 +74,23 @@ const UserProfile = () => {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<main className="flex-grow">
-				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-0 py-8">
 					<h1 className="text-3xl font-bold text-gray-800 mb-4">
 						User Profile
 					</h1>
 					{userData && (
-						<div className="flex flex-wrap items-start justify-between gap-5 bg-white p-6 rounded-lg shadow-md">
-							<div className="lg:basis-3/12 flex flex-col gap-4 text-start">
+						<div className="flex flex-wrap items-start justify-start gap-5">
+							<div className="w-full lg:basis-3/12 flex flex-col gap-4 text-start bg-white p-6 rounded-lg shadow-md">
 								<p className="text-2xl font-semibold text-gray-800">
-									{userData.username}
+									Username: {userData.username}
 								</p>
 								<p className="text-gray-600">
-									{userData.email}
+									Email: {userData.email}
 								</p>
-								<p className="text-gray-600">{user.phone}</p>
+								<p className="text-gray-600">
+									Contact Number:{" "}
+									{userData?.vendorDetails?.contact}
+								</p>
 								<p className="text-gray-600">
 									School: {user.school}
 								</p>
@@ -106,30 +110,102 @@ const UserProfile = () => {
 								</div>
 							</div>
 
-							<div className="lg:basis-8/12">
+							<div className="w-full lg:-mt-12 lg:basis-8/12">
 								<h2 className="text-2xl font-semibold text-gray-800 mb-4">
 									Catalogue
 								</h2>
-								<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-8">
-									{postedProducts.map((product) => (
-										<div
-											key={product.id}
-											className="bg-gray-100 p-4 rounded-lg shadow-md"
+								<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-2">
+									{userData.products.map((product) => (
+										<Link
+											to={`/product/${product._id}`}
+											key={product._id}
+											className="relative flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
 										>
-											<img
-												src={product.image}
-												alt={product.name}
-												className="w-full h-56 object-cover object-center"
-											/>
-											<div className="mt-4">
-												<h3 className="text-xl font-semibold text-gray-800">
-													{product.name}
-												</h3>
-												<p className="text-gray-600 mt-2">
-													{product.price}
+											<div className="flex items-center justify-center">
+												<p className="mx-0 sm:mx-0 mt-3 flex h-36 sm:h-40 overflow-hidden rounded-xl">
+													<img
+														className="object-cover"
+														src={
+															product.imageUrls[0]
+														}
+														alt="product image"
+													/>
 												</p>
 											</div>
-										</div>
+											<div className="mt-4 px-2 sm:px-4 pb-3 sm:pb-5">
+												<h5 className="text-lg sm:text-xl tracking-tight text-slate-900 line-clamp-1">
+													{product.name}
+												</h5>
+												<div className="mt-0 sm:mt-2 mb-1 sm:mb-5 flex items-center justify-between">
+													<p>
+														<span className="text-md sm:text-xl lg:text-2xl font-bold text-slate-900">
+															₵{product.price}
+														</span>
+														<span className="text-xs sm:text-sm text-slate-900 line-through">
+															₵
+															{(
+																product.price -
+																78
+															).toFixed(2)}
+														</span>
+													</p>
+													{/* <div className="flex items-center">
+													<svg
+														aria-hidden="true"
+														className="h-5 w-5 text-yellow-300"
+														fill="currentColor"
+														viewBox="0 0 20 20"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+													</svg>
+													<svg
+														aria-hidden="true"
+														className="h-5 w-5 text-yellow-300"
+														fill="currentColor"
+														viewBox="0 0 20 20"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+													</svg>
+													<svg
+														aria-hidden="true"
+														className="h-5 w-5 text-yellow-300"
+														fill="currentColor"
+														viewBox="0 0 20 20"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+													</svg>
+													<svg
+														aria-hidden="true"
+														className="h-5 w-5 text-yellow-300"
+														fill="currentColor"
+														viewBox="0 0 20 20"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+													</svg>
+													<svg
+														aria-hidden="true"
+														className="h-5 w-5 text-yellow-300"
+														fill="currentColor"
+														viewBox="0 0 20 20"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+													</svg>
+													<span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+														5.0
+													</span>
+												</div> */}
+												</div>
+												<button className="w-full flex items-center justify-center gap-2 rounded-md bg-blue-900 px-2 sm:px-5 py-2 text-center text-xs sm:text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
+													<CiViewList className="text-xl font-bold" />
+													View details
+												</button>
+											</div>
+										</Link>
 									))}
 								</div>
 							</div>

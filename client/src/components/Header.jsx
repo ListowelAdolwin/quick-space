4d47586719ded479cb44fc31ea87e3b41233ff9d
ListowelAdolwin <listoweladolwin>
2024-05-27 13:14:58 +0000

    Updated mobile view menu to open from the right

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { FaRegHeart, FaRegUserCircle } from "react-icons/fa";
@@ -11,6 +11,20 @@ const Header = () => {
 
 	const { currentUser } = useSelector((state) => state.user);
 
+	const handleOverlayClick = () => {
+		setIsOpen(false);
+	};
+
+	const handleMenuClick = (event) => {
+		if (event.target.tagName === "A") {
+			setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		setIsOpen(false)
+	}, [])
+
 	return (
 		<nav className="bg-white shadow-md">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -20,7 +34,7 @@ const Header = () => {
 							className="hidden sm:inline-block sm:w-32 sm:h-auto md:w-40 md:h-16"
 							src={logo_desktop}
 							alt="Logo"
-						/> 
+						/>
 						<img
 							className="sm:hidden w-12 h-12"
 							src={logo_mobile}
@@ -124,7 +138,7 @@ const Header = () => {
 						<button
 							onClick={() => setIsOpen(!isOpen)}
 							type="button"
-							className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white  hover:outline-none hover:ring-1 hover:ring-offset-1 hover:ring-offset-gray-800 hover:ring-white"
+							className="z-50 inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white  hover:outline-none hover:ring-1 hover:ring-offset-1 hover:ring-offset-gray-800 hover:ring-white"
 						>
 							<span className="sr-only">Open main menu</span>
 							<svg
@@ -167,81 +181,96 @@ const Header = () => {
 				</div>
 			</div>
 
-			<div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
-				<div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-					<Link
-						to="/"
-						className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
+			{/* Overlay to close mobile view menu */}
+			{isOpen && (
+				<div className="fixed inset-0 z-40 flex">
+					<div
+						className="bg-black bg-opacity-50 flex-grow"
+						onClick={handleOverlayClick}
+					></div>
+					<div
+						onClick={handleMenuClick}
+						className="bg-white w-3/4 max-w-sm shadow-lg transform translate-x-0 transition-transform duration-300 ease-in-out pt-4 overflow-scroll"
 					>
-						Home
-					</Link>
-					<Link
-						to="/shop"
-						className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
-					>
-						Shop
-					</Link>
-					<Link
-						to="/about"
-						className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
-					>
-						About
-					</Link>
-					{currentUser ? (
-						<div>
+						<div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
 							<Link
-								to="/add-product"
+								to="/"
 								className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
 							>
-								Add Product
+								Home
 							</Link>
 							<Link
-								to="/favourites"
+								to="/shop"
 								className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
 							>
-								<FaRegHeart
-									className="inline-block"
-									size={20}
-								/>
+								Shop
 							</Link>
 							<Link
-								to="/cart"
+								to="/about"
 								className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
 							>
-								<FiShoppingCart
-									className="inline-block"
-									size={20}
-								/>
+								About
 							</Link>
-							<Link
-								to={`/profile/${currentUser._id}`}
-								className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
-							>
-								<FaRegUserCircle
-									className="inline-block"
-									size={20}
-								/>
-							</Link>
+							{currentUser ? (
+								<div>
+									<Link
+										to="/add-product"
+										className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Add Product
+									</Link>
+									<Link
+										to="/favourites"
+										className="flex items-center  gap-2 text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-base font-medium"
+									>
+										<FaRegHeart
+											className="inline-block"
+											size={20}
+										/>
+										Favorites
+									</Link>
+									<Link
+										to="/cart"
+										className="flex items-center  gap-2 text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-base font-medium"
+									>
+										<FiShoppingCart
+											className="inline-block"
+											size={20}
+										/>
+										Cart
+									</Link>
+									<Link
+										to={`/profile/${currentUser._id}`}
+										className="flex items-center  gap-2 text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-base font-medium"
+									>
+										<FaRegUserCircle
+											className="inline-block"
+											size={20}
+										/>
+										Profile
+									</Link>
+								</div>
+							) : (
+								<div className="flex flex-col">
+									{" "}
+									<Link
+										to="login"
+										className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
+									>
+										Login
+									</Link>{" "}
+									<Link
+										to="register"
+										className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
+									>
+										Register
+									</Link>
+								</div>
+							)}
 						</div>
-					) : (
-						<div className="flex flex-col">
-							{" "}
-							<Link
-								to="login"
-								className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
-							>
-								Login
-							</Link>{" "}
-							<Link
-								to="register"
-								className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
-							>
-								Register
-							</Link>
-						</div>
-					)}
+					</div>
 				</div>
-			</div>
+			)}
 		</nav>
 	);
 };

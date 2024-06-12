88f3bf2f367d2158c6fd794ad1fece2a0f4071d4
ListowelAdolwin <listoweladolwin@gmail.com>
2024-06-12 12:09:54 +0000

    Updated cors

--- api/server.js
+++ api/server.js
@@ -17,13 +17,13 @@ app.use(express.json())
 app.use(
 	cors({
 		origin: [
-			"http://127.0.0.1:5173/",
-			"https://127.0.0.1:5173/",
 			"http://127.0.0.1:5173",
-			"http://quick-space-xyz.onrender.com",
-			"http://quick-space-xyz.onrender.com/",
-			"https://quick-space-xyz.onrender.com/",
-			"https://quick-space-xyz.onrender.com",
+      "https://127.0.0.1:5173",
+			'http://localhost:3000',
+			'http://127.0.0.1:3000',
+      "http://quick-space-xyz.onrender.com",
+      "https://quick-space-xyz.onrender.com",
+      "https://quickspace.tertiaryguide.com",
 		],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
